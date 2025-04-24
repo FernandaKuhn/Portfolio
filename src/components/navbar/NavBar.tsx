a6da0d8198cbbefd7f12c43960537e41407b8ac0
Fernanda Kuhn <fernandakuhn1@hotmail.com>
2025-04-24 15:44:26 -0300

    fix/orange-img

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,9 +1,10 @@
 import { AppBar, MenuItem, styled } from "@mui/material";
 import { Link } from "react-scroll";
-import orange from "D:/estudo/portifolio/Portfolio/src/assets/images/orange.png";
+import orange from "../../assets/images/orange.png";
+
 
 const NavBar = () => {
-    const StyledToolbar = styled("div")`
+  const StyledToolbar = styled("div")`
     position: fixed;
     background-color: #3c3c3c7a;
     height: 70px;
@@ -14,18 +15,18 @@ const NavBar = () => {
     z-index: 999;
   `;
 
-    const ScrollLink = styled(Link)`
+  const ScrollLink = styled(Link)`
     text-decoration: none;
     color: inherit;
     cursor: pointer;
   `;
 
-    const StyledMenuItem = styled(MenuItem)`
+  const StyledMenuItem = styled(MenuItem)`
     font-weight: bold;
     letter-spacing: 1px;
   `;
 
-    const Logo = styled("img")`
+  const Logo = styled("img")`
     height: 45px;
     cursor: pointer;
     display: flex;
@@ -33,24 +34,24 @@ const NavBar = () => {
     user-select: none;
   `;
 
-    return (
-        <AppBar position="absolute" elevation={0}>
-            <StyledToolbar>
-                <ScrollLink to="hero" smooth={true} duration={500} offset={-80}>
-                    <Logo src={orange} alt="Logo Orange" />
-                </ScrollLink>
-                <ScrollLink to="aboutme" smooth={true} duration={500} offset={-80}>
-                    <StyledMenuItem>Sobre mim</StyledMenuItem>
-                </ScrollLink>
-                <ScrollLink to="technologies" smooth={true} duration={500} offset={-80}>
-                    <StyledMenuItem>Tecnologias</StyledMenuItem>
-                </ScrollLink>
-                <ScrollLink to="projects" smooth={true} duration={500} offset={-80}>
-                    <StyledMenuItem>Projetos</StyledMenuItem>
-                </ScrollLink>
-            </StyledToolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar position="absolute" elevation={0}>
+      <StyledToolbar>
+        <ScrollLink to="hero" smooth={true} duration={500} offset={-80}>
+          <Logo src={orange} alt="Logo Orange" />
+        </ScrollLink>
+        <ScrollLink to="aboutme" smooth={true} duration={500} offset={-80}>
+          <StyledMenuItem>Sobre mim</StyledMenuItem>
+        </ScrollLink>
+        <ScrollLink to="technologies" smooth={true} duration={500} offset={-80}>
+          <StyledMenuItem>Tecnologias</StyledMenuItem>
+        </ScrollLink>
+        <ScrollLink to="projects" smooth={true} duration={500} offset={-80}>
+          <StyledMenuItem>Projetos</StyledMenuItem>
+        </ScrollLink>
+      </StyledToolbar>
+    </AppBar>
+  );
 };
 
 export default NavBar;
